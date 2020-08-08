9ce2b55fba03375d013b10abe7038d4489a39c7e
Dmitriy Balashov <mockedcat@gmail.com>
2020-08-08 21:38:42 +0300

    brain calc getCorrectAnswer refactored

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -2,25 +2,28 @@ import pairs from '@hexlet/pairs';
 import utils from '../utils.js';
 import playGameFlow from '../index.js';
 
-function generateQuestionAnswerPair() {
-  const operators = ['+', '-', '*'];
-  function getCorrectAnswer(question) {
-    const [operandA, operator, operandB] = question.split(' ');
-
-    switch (operator) {
-      case operators[0]:
-        return Number(operandA) + Number(operandB);
-      case operators[1]:
-        return Number(operandA) - Number(operandB);
-      case operators[2]:
-        return Number(operandA) * Number(operandB);
-      default:
-        return null;
-    }
+const operators = ['+', '-', '*'];
+function getCorrectAnswer(operandA, operator, operandB) {
+  switch (operator) {
+    case operators[0]:
+      return operandA + operandB;
+    case operators[1]:
+      return operandA - operandB;
+    case operators[2]:
+      return operandA * operandB;
+    default:
+      return null;
   }
+}
 
-  const question = `${utils.getRandomInt(1, 10)} ${operators[utils.getRandomInt(0, 2)]} ${utils.getRandomInt(1, 10)}`;
-  const answer = String(getCorrectAnswer(question));
+function generateQuestionAnswerPair() {
+  const [operandA, operator, operandB] = [
+    utils.getRandomInt(1, 10),
+    operators[utils.getRandomInt(0, 2)],
+    utils.getRandomInt(1, 10),
+  ];
+  const question = `${operandA} ${operator} ${operandB}`;
+  const answer = String(getCorrectAnswer(operandA, operator, operandB));
 
   return pairs.cons(question, answer);
 }
